656c9da6dd9771950b4f8c3769befd6a845ef38e
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2016-06-28 09:41:59 +0100

    Refactor HW04

--- HW04.hs
+++ HW04.hs
@@ -24,23 +24,18 @@ instance (Num a, Eq a) => Eq (Poly a) where
 -- Exercise 3 -----------------------------------------
 
 instance (Num a, Eq a, Show a) => Show (Poly a) where
-    -- show (P []) = ""
-    -- show (P (x:[])) = show x
-    -- show (P s@(x:xs)) = coefficient ++ show (P $ init s)
-    --     where coefficient = if (last xs /= 0) then (if (last xs /= 1 && last xs /= (-1)) then show (last xs) else "") ++ "x" ++ exponent ++ " + " else ""
-    --           exponent = if (length xs > 1) then ( "^" ++ (show $ length xs)) else ""
-    show (P []) = show 0
+    show (P []) = "0"
     show (P xs) = let coefficients = zip xs [0..]
                       non0Coefficients = filter (\(c,_) -> c /= 0) coefficients
                       coefficient c
                         | c == 1    = ""
                         | otherwise = show c
-                      exponent e = case e of 0 -> ""
-                                             1 -> "x"
-                                             _ -> "x^" ++ show e
+                      exponent' e = case e of 0 -> ""
+                                              1 -> "x"
+                                              _ -> "x^" ++ show e
                       display (c,e)
-                        | c == 1 && e == 0 = show 1
-                        | otherwise        = coefficient c ++ exponent e
+                        | c == 1 && e == 0 = "1"
+                        | otherwise        = coefficient c ++ exponent' e
                   in  intercalate " + " $ reverse $ map display non0Coefficients
 
 -- Exercise 4 -----------------------------------------
@@ -56,7 +51,7 @@ plus (P xs) (P ys) = P $ zipWith (+) xsPad ysPad
 
 times :: Num a => Poly a -> Poly a -> Poly a
 times (P xs) (P ys) = let coefficients = zip xs [0..]
-    in foldl plus (P []) $ map P $ [z | (x,c) <- coefficients, let z = map (*x) (replicate c 0 ++ ys)]
+    in foldl plus (P []) $ map P [z | (n,c) <- coefficients, let z = map (*n) (replicate c 0 ++ ys)]
 
 -- Exercise 6 -----------------------------------------
 
@@ -66,7 +61,7 @@ instance Num a => Num (Poly a) where
     -- negate :: Num a => Poly a -> Poly a
     negate (P xs) = P $ map negate xs
     -- fromInteger :: Num a => Integer -> Poly a
-    fromInteger x = P [fromInteger x]
+    fromInteger n = P [fromInteger n]
     -- No meaningful definitions exist
     abs    = undefined
     signum = undefined
@@ -74,8 +69,7 @@ instance Num a => Num (Poly a) where
 -- Exercise 7 -----------------------------------------
 
 applyP :: Num a => Poly a -> a -> a
-applyP (P xs) x = let exponents = zip xs [0..]
-                  in  sum $ map (\(c,e) -> c * x ^ e) exponents
+applyP (P xs) n = sum $ zipWith (\c e -> c * n ^ e) xs [0..]
 
 -- Exercise 8 -----------------------------------------
 
@@ -89,5 +83,4 @@ class Num a => Differentiable a where
 
 instance (Enum a, Num a) => Differentiable (Poly a) where
     deriv (P []) = P []
-    deriv (P xs) = let exponents = zip xs [0..]
-                   in  P (tail $ map (uncurry (*)) exponents)
+    deriv (P xs) = P (tail $ zipWith (*) xs [0..])
